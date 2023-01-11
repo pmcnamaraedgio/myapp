c4475cc4367d5a514bfec42317a863c5fb7aa8ba
pmcnamaraedgio <109738432+pmcnamaraedgio@users.noreply.github.com>
2023-01-11 14:22:11 +0000

    my latest change

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Hey Paul!!!!. Get started by editing&nbsp;
+            Hi Paul!!!!. Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
         </div>
